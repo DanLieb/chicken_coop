0c542d98484e8aed489e2a17d4d84d63084c7486
Daniel Liebmann <5285322+DanLieb@users.noreply.github.com>
2022-10-26 00:05:24 +0200

    Splitted Files and made preps for the integration  of the rest api

---
+++ chicken_coop_rest.py
@@ -0,0 +1,5 @@
+
+
+class ChickeChickenCoopRestServer:
+    def __init__ (self): 
+        pass
--- main.py
+++ main.py
@@ -4,72 +4,22 @@ import time
 import logging
 import threading
 
-import settings
+# import settings
+from chicken_coop_commander import ChickenCoopCommander
 from chicken_coop import ChickenCoop
 
 
-class ChickenCoopCommander:
-    
-    def __init__(self, cc):
-        self.__cc = cc
-        
-        self.__temp_thread = threading.Thread(target=self.runTemperatureManagement)
-        self.__temp_stop = threading.Event()
-        
-        self.__light_thread = threading.Thread(target=self.runLightManagement)
-        self.__light_stop = threading.Event()
-        
-    def runTemperatureManagement(self):
-        while True:
-            current_temperature = self.__cc.getTemperature()
-            logging.info("Temperatur bei %.2f", current_temperature)
-            
-            if (current_temperature <= settings.temperature_low and self.__cc.isHeating() == False):
-                self.__cc.heatingOn()
-                logging.info("Wärmelampe Ein bei %.2f", current_temperature)
-            elif (current_temperature >= settings.temperature_high and self.__cc.isHeating() == True):
-                self.__cc.heatingOff()
-                logging.info("Wärmelampe Aus bei %.2f", current_temperature)
-                
-            if self.__temp_stop.is_set():
-                break
-            time.sleep(settings.timing_temperature)
-            
-    def runLightManagement(self):
-        while True:
-            current_brightness = self.__cc.getBrightness()
 
-            if self.__cc.isLight() == True:
-                self.__cc.lightOff()
-            else:
-                self.__cc.lightOn()    
-            
-
-            if self.__light_stop.is_set():
-                break
-            
-            time.sleep(settings.timing_brightness)
-            
-    def run(self):
-        logging.info("Starting Light & Temperature Management")
-        
-        self.__temp_thread.start()
-        self.__light_thread.start()
-        
-        logging.info("Starting Light & Temperature Management Done!")
-        
-    def __del__(self):
-        self.__temp_stop.set()
-        self.__temp_thread.join()
-        
-        self.__light_stop.set()
-        self.__light_thread.join()
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%Y-%m-%D %H:%M:%S")
+    logging.basicConfig(
+        format=format, 
+        level=logging.INFO,
+        datefmt="%Y-%m-%D %H:%M:%S",
+        filename="output.log")
 
     logging.info("Starting in __main__")
     
