b4bfe5c3be9865f9f7b66ac9f3ae00b3d0e25e9f
Daniel Liebmann <5285322+DanLieb@users.noreply.github.com>
2022-11-04 23:20:48 +0100

    Changed Data Logging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 import threading
 from flask import Flask
 
-# import settings
+import settings
 from chicken_coop_commander import ChickenCoopCommander
 from chicken_coop import ChickenCoop
 
@@ -14,15 +14,30 @@ from chicken_coop import ChickenCoop
 
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
+    
+
+    
+    data_formatter = logging.Formatter('%(asctime)s %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    
+    temp_logger = logging.getLogger("Temperature_Sensor")
+    temp_handler = logging.FileHandler(settings.TEMP_LOGS)
+    temp_handler.setFormatter(data_formatter)
+    temp_logger.setLevel(logging.INFO)
+    temp_logger.addHandler(temp_handler)
+    
+    brightness_logger = logging.getLogger("Brightness_Sensor")
+    brightness_handler = logging.FileHandler(settings.TEMP_LOGS)
+    brightness_handler.setFormatter(data_formatter)
+    brightness_logger.setLevel(logging.INFO)
+    brightness_logger.addHandler(temp_handler)
     
     logging.basicConfig(
-        format=format, 
+        format="%(asctime)s: %(message)s", 
         level=logging.INFO,
         datefmt="%Y-%m-%D %H:%M:%S")
         # filename="output.log")
 
-    logging.info("Starting in __main__")
+    logging.info("Starting Up The ChickenScoop Module")
     
     try:
         cc = ChickenCoop()
