7e8e073070f6a8d8107fbbee0c7979eb1ee2c901
Daniel Liebmann <5285322+DanLieb@users.noreply.github.com>
2022-11-04 21:51:26 +0100

    trial an error

--- chicken_coop.py
+++ chicken_coop.py
@@ -145,23 +145,9 @@ class ChickenCoop:
         return current_temperature
     
     def getBrightness(self):
-        # print("getBrightness(): handle = %i" % self.brightness_sensor)
-        # (count, data) = self.pi.i2c_read_i2c_block_data(self.brightness_sensor, 0x20, 2)
-        # print("getBrightness(): count: %i data:" % count)
-        
-        # if count >= 0:
-        #     pass
-        # else:
-            
-        #     print("%s" % pigpio.error_text(count))
-            
-        
-        #data = self.pi.i2c_read_word_data(self.brightness_sensor, 0x20)
-        #print("data = %i" % data)
-        #count = data >> 8 | (data & 0xff) << 8
-        #print("count = %i" % count)
-        # brightness = count / 1.2
-        return self._bsensor.measure_high_res()
+        b = self._bsensor.measure_high_res()
+        print("Helligkeit: %.2f lx" % b)
+        return b
         
     def isHeating(self):
         return self.heating
