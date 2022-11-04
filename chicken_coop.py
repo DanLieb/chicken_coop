5bfeab49d0352249f27dde8e9521eea74ca7a7da
Daniel Liebmann <5285322+DanLieb@users.noreply.github.com>
2022-11-04 21:00:45 +0100

    asdfasdf

--- chicken_coop.py
+++ chicken_coop.py
@@ -137,6 +137,8 @@ class ChickenCoop:
         return current_temperature
     
     def getBrightness(self):
+        
+        print("getBrightness(): handle = %i" % self.brightness_sensor)
         data = self.pi.i2c_read_word_data(self.brightness_sensor, 0x20)
         print("data = %i" % data)
         count = data >> 8 | (data & 0xff) << 8
